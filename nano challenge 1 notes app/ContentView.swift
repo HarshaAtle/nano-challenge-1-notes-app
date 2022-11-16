1774190fcafce2314b081e85a8f13d18ebd625d9
HarshaAtle <94773341+HarshaAtle@users.noreply.github.com>
2022-11-16 15:08:33 +0100

    Initial Commit

---
+++ nano challenge 1 notes app/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  nano challenge 1 notes app
+//
+//  Created by Harsha Vardhan on 16/11/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ nano challenge 1 notes app/nano_challenge_1_notes_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  nano_challenge_1_notes_appApp.swift
+//  nano challenge 1 notes app
+//
+//  Created by Harsha Vardhan on 16/11/22.
+//
+
+import SwiftUI
+
+@main
+struct nano_challenge_1_notes_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
