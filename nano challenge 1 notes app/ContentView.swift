a2fcc39ae15fa02733241fa6ad1a7d87f2c6407b
HarshaAtle <94773341+HarshaAtle@users.noreply.github.com>
2022-11-17 16:41:53 +0100

    Initial Commit for my project

--- nano challenge 1 notes app/ContentView.swift
+++ nano challenge 1 notes app/ContentView.swift
@@ -8,19 +8,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    var folders = ["Folder1","Folder2"]
+    @State var searchString = ""
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        NavigationView(){
+            List {
+                TextField("Search", text: $searchString)
+                Section(header:
+                            Text("iCloud")
+                    .font(.title3)
+                    .fontWeight(.bold)
+                    .foregroundColor(.black)) {
+                        ForEach(folders, id: \.self){ folderName in
+                            FolderCell(name: folderName)
+                        }
+                    }
+                
+            }    .textCase(nil)
+                .listStyle(InsetGroupedListStyle())
+                .navigationTitle("folders")
+                .toolbar {
+                    ToolbarItemGroup(placement: .navigationBarTrailing){
+                            Button("Edit"){
+                                print("Edit")
+                        }
+                    }
+                    ToolbarItemGroup(placement: .bottomBar){
+                        Image(systemName: "folder.badge.plus")
+                        Image(systemName: "sqaure.and.pencil")
+                    }
+            }
         }
-        .padding()
     }
 }
+    
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
+    }
+    
+    struct FolderCell: View {
+        var name: String
+        var body: some View {
+            HStack {
+                Image(systemName: "folder")
+                Text(name)
+            }
+        }
+    }
+    
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
---
+++ nano challenge 1 notes app/MainView.swift
@@ -0,0 +1,11 @@
+//
+//  MainView.swift
+//  nano challenge 1 notes app
+//
+//  Created by Harsha Vardhan on 16/11/22.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    
